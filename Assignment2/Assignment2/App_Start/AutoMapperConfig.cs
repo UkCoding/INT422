3417dde53007fa9352b912c420b33c251bc28bf0
ukCoding <ukj512@gmail.com>
2018-04-27 20:04:45 -0400

    assignment2 added

---
+++ Assignment2/Assignment2/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Assignment2
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // Add map creation statements here
+            // Mapper.CreateMap< FROM , TO >();
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Controllers.EmployeeAdd, Models.Employee>());
+            Mapper.Initialize(cfg => cfg.CreateMap<Models.Employee, Controllers.EmployeeBase>());
+        }
+    }
+}
